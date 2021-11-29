c52699e4f10c051a84460fb3a14d1b0b1030c043
Amos <amosluo108@gmail.com>
2021-11-29 16:11:23 -0500

    made it repeat

--- main.cpp
+++ main.cpp
@@ -6,32 +6,45 @@
 #include "std_lib_facilities.h"
 
 int main() {
-	
+
 	string operation;
 	double num1;
 	double num2;
 	double result{};
+	int x = 1;
 
-	cout << "Enter operation: \n";
-	cin >> operation;
-	cout << "Enter first operand: \n";
-	cin >> num1;
-	cout << "Enter second operand: \n";
-	cin >> num2;
-	
-	if (operation == "+" || operation == "add") {
-		result = num1 + num2;
-	}
-	if (operation == "-" || operation == "minus") {
-		result = num1 - num2;
-	}
-	if (operation == "*" || operation == "mul") {
-		result = num1 * num2;
-	}
-	if (operation == "/" || operation == "div") {
-		result = num1 / num2;
-	}
+	while (x = 1) {
 
-	cout << "Result is " << result << "";
+		cout << "Enter operation: \n";
+		cin >> operation;
+		cout << "Enter first operand: \n";
+		cin >> num1;
+		cout << "Enter second operand: \n";
+		cin >> num2;
 
-}
+			if (operation == "+" || operation == "add") {
+				result = num1 + num2;
+				cout << "Result is " << result << "\n";
+			}
+			if (operation == "-" || operation == "minus") {
+				result = num1 - num2;
+				cout << "Result is " << result << "\n";
+			}
+			if (operation == "*" || operation == "mul") {
+				result = num1 * num2;
+				cout << "Result is " << result << "\n";
+			}
+			if (operation == "/" || operation == "div") {
+
+				result = num1 / num2;
+
+				if (num1 == 0 || num2 == 0) {
+					cout << "Cannot divide by zero";
+					exit;
+				}
+
+				cout << "Result is " << result << "\n";
+			}
+
+		}
+	}
