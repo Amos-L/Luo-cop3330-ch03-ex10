43b94bbd487acab31f567b87c197d177806158d6
Amos <amosluo108@gmail.com>
2021-11-29 15:57:55 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Amos Luo
+ */
+
+#include "std_lib_facilities.h"
+
+int main() {
+	
+	string operation;
+	double num1;
+	double num2;
+	double result{};
+
+	cout << "Enter operation: \n";
+	cin >> operation;
+	cout << "Enter first operand: \n";
+	cin >> num1;
+	cout << "Enter second operand: \n";
+	cin >> num2;
+	
+	if (operation == "+" || operation == "add") {
+		result = num1 + num2;
+	}
+	if (operation == "-" || operation == "minus") {
+		result = num1 - num2;
+	}
+	if (operation == "*" || operation == "mul") {
+		result = num1 * num2;
+	}
+	if (operation == "/" || operation == "div") {
+		result = num1 / num2;
+	}
+
+	cout << "Result is " << result << "";
+
+}
